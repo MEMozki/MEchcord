da952fb15dd9a94bc45d6f5c09fb45b001144a68
F3Mozk1_ <memozki@bk.ru>
2025-03-20 21:23:02 +0300

    Create __init__.py

---
+++ mechcord/__init__.py
@@ -0,0 +1,19 @@
+from .client import Client
+from .commands import command
+from . import client
+from . import commands
+from . import models
+from . import http
+from . import websocket
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "Client",
+    "command",
+    "client",
+    "commands",
+    "models",
+    "http",
+    "websocket"
+]
